2efbc01f8d0544e894540ba1f2736429abc5186f
ka <ka@192.168.0.101>
2024-09-09 11:15:27 -0400

    segundo commit

---
+++ js/pract.js
@@ -0,0 +1,20 @@
+console.log("Hello".trim());
+// agregar un valor a un localStorage
+localStorage.setItem('sesion', 'si');
+console.log(localStorage.getItem('sesion'));
+
+<input type="text" onkeyup="this.value = this.value.upperCase();"></input>
+//unción para que retorne el doble del número pasado como parámetro
+function doble(x){
+    return x*2;
+}
+//código para crear un objeto con las propiedades nombre y edad
+var persona = {
+    nombre: 'Juan',
+    edad: 30
+}
+
+//mensaje de alerta cuando se haga clic en un botón
+<button onclick="alert('Hola')">Hola</button>
+//ódigo para seleccionar un elemento con el ID miDiv y cambiar su contenido
+document.getElementById("miDiv").innerHTML = "Hola";
